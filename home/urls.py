e259ff5c0234497580ffd875ec4db4b31e4dc007
girisabin <girisabin79@gmail.com>
2022-04-25 09:16:22 +0545

    second commit

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,8 @@ from .views import *
 app_name = "home"
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('detail/<slug>', ProductDetailView.as_view(), name='detail'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('subcategory/<slug>', SubCategoryView.as_view(), name='subcategory')
 ]
